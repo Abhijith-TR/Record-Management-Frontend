e6c9cba352c5cf5187654c11a546e96ab414d54d
Abhijith T R <2020csb1062@iitrpr.ac.in>
2022-06-27 10:48:16 +0530

    Added Requests for Records

--- src/View.js
+++ src/View.js
@@ -1,31 +1,88 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const View = () => {
-  const [entryNumber, setEntryNumber] = useState("");
-  const [subjectCode, setSubjectCode] = useState("");
+  const [records, setRecords] = useState([]);
+  const [subjectCode, setSubjectCode] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [error, setError] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
 
-  const getRecords = async () => {};
+  useEffect(() => {
+    if (error) {
+      const timeout = setTimeout(() => {
+        setError(false);
+      }, 3000);
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
+  }, [error]);
+
+  const getRecords = async (e) => {
+    console.log(subjectCode);
+    e.preventDefault();
+    if (!searchValue) {
+      setError(true);
+      setErrMsg("Invalid Input");
+    } else if (!subjectCode) {
+      try {
+        const { data } = await axios.get(
+          `https://irms-server.herokuapp.com/api/admin/records/get/${searchValue}`,
+          {
+            headers: {
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbklkIjoiNjJiNDdkNTM4YzhkMWJlZDdjZjc3MTY0IiwibmFtZSI6IkpvaG4gRG9lIiwiaXNBZG1pbiI6dHJ1ZSwiaXNTdXBlciI6dHJ1ZSwiaWF0IjoxNjU2MzA1NDYyLCJleHAiOjE2NTYzNDE0NjJ9.wqfM_z25uxanGzYwwE-eSyxO5gjtFSN3JY-lD_sTivs",
+            },
+          }
+        );
+        setRecords(data.records);
+        console.log(records);
+      } catch (error) {
+        setError(true);
+        setErrMsg(error.response.data.msg);
+      }
+    } else {
+      try {
+        const { data } = await axios.get(
+          `https://irms-server.herokuapp.com/api/admin/records/${searchValue}`,
+          {
+            headers: {
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbklkIjoiNjJiNDdkNTM4YzhkMWJlZDdjZjc3MTY0IiwibmFtZSI6IkpvaG4gRG9lIiwiaXNBZG1pbiI6dHJ1ZSwiaXNTdXBlciI6dHJ1ZSwiaWF0IjoxNjU2MzA1NDYyLCJleHAiOjE2NTYzNDE0NjJ9.wqfM_z25uxanGzYwwE-eSyxO5gjtFSN3JY-lD_sTivs",
+            },
+          }
+        );
+        setRecords(data.records);
+        console.log(records);
+      } catch (error) {
+        setError(true);
+        setErrMsg(error.response.data.msg);
+      }
+    }
+    setSearchValue("");
+  };
 
   return (
     <>
       <div>
         <h2 className="page-headings">Display Records</h2>
-        <form className="record-search">
+        <form className="record-search" onSubmit={getRecords}>
           <input
             type="text"
             className="login-input login-input-alt"
-            placeholder="Enter entry number"
-            onChange={(e) => setEntryNumber(e.target.value)}
-            value={entryNumber}
+            placeholder="Entry Number / Subject Code"
+            onChange={(e) => setSearchValue(e.target.value)}
+            value={searchValue}
           />
-          <input
-            type="text"
-            className="login-input login-input-alt"
-            placeholder="Enter subject code"
-            onChange={(e) => setSubjectCode(e.target.value)}
-            value={subjectCode}
-            style={{ marginTop: "0.5rem" }}
-          />
+          <label style={{ textAlign: "center", marginTop: "0.4rem" }}>
+            Subject Code
+            <input
+              type="checkbox"
+              style={{ marginLeft: "0.5rem", marginTop: "0.2rem" }}
+              onChange={() => setSubjectCode(!subjectCode)}
+            />
+          </label>
           <button
             type="submit"
             className="submit-btn submit-btn-alt"
@@ -34,6 +91,7 @@ const View = () => {
             Search
           </button>
         </form>
+        <div className="error">{error ? errMsg : ""}</div>
       </div>
     </>
   );
